57215669cfdc3f1e958595195c92ecbef304488d
dheeraj5999 <dheeraj.pasupuleti99>
2024-01-05 10:59:39 +0530

    ADDED

--- target/generated-sources/htl/org/apache/sling/scripting/sightly/apps/wknd/components/testing/testing__002e__html.java
+++ target/generated-sources/htl/org/apache/sling/scripting/sightly/apps/wknd/components/testing/testing__002e__html.java
@@ -34,7 +34,7 @@ public final class testing__002e__html extends RenderUnit {
                                 RenderContext renderContext) {
 // Main Template Body -----------------------------------------------------------------------------
 
-out.write("<p>This is Testing</p>\n<p>A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic. Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs.</p>");
+out.write("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN\" crossorigin=\"anonymous\"/>\n<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL\" crossorigin=\"anonymous\"></script>\n");
 
 
 // End Of Main Template Body ----------------------------------------------------------------------
